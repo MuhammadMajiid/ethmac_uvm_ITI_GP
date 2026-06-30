2bac9ec3a7a4566d7bc09c5faa079e30c728f3a2
Mohammad Wael <mwael5536@gmail.com>
2026-06-30 16:19:52 +0300

    created eth tx scoreboard

---
+++ tb/Scoreboards/eth_tx_scoreboard.sv
@@ -0,0 +1,117 @@
+//==============================================================================
+// Project  : ethmac_uvm_ITI_GP
+// File     : eth_tx_scoreboard.sv
+// Author   : Wael
+// Date     : 2026-06-30
+//------------------------------------------------------------------------------
+// Description:
+//   Ethernet scoreboard responsible for implementation of golden model of tx
+//   and comparing it's expected packet with the actual tx packet. 
+//==============================================================================
+`ifndef ETH_TX_SCOREBOARD_SV
+`define ETH_TX_SCOREBOARD_SV
+class eth_tx_scoreboard extends uvm_scoreboard;
+    `uvm_component_utils(eth_tx_scoreboard)
+
+    // =========================================================================
+    // Analysis fifos — one for wishbone master, one for MII TX
+    // =========================================================================
+    uvm_analysis_fifo  #(wb_m_seq_item_base)        wb_m_fifo;
+    uvm_analysis_fifo  #(mii_tx_seq_item_base)      mii_tx_fifo;
+    // =========================================================================
+    // Analysis exports — one for wishbone master, one for MII TX
+    // =========================================================================
+    uvm_analysis_fifo  #(wb_m_seq_item_base)        wb_m_a_export;
+    uvm_analysis_fifo  #(mii_tx_seq_item_base)      mii_tx_a_export;
+    // wb_m transaction for storing last item pulled from wb_m fifo
+    wb_m_seq_item_base                              m_wb_m_seq_item;
+    // mii_tx transaction for storing last item pulled from mii_tx fifo
+    mii_tx_seq_item_base                            m_mii_tx_seq_item_base;
+
+    // =========================================================================
+    // Constructor, Build Phase, Connect phase and Run phase
+    // =========================================================================
+    extern function new(string name, uvm_component parent);
+    extern function void build_phase(uvm_phase phase);
+    extern function void connect_phase(uvm_phase phase);
+    extern task run_phase(uvm_phase phase);
+    // -------------------------------------------------------------------------
+    //  task : predictor
+    // -------------------------------------------------------------------------
+    // Description:
+    //   Implements golden model of tx, Construct packet and send it to
+    //   comparartor.Detects collision, carrier sense errors like underrun and
+    //   huge packet.   
+    // Arguments: None
+    //
+    // -------------------------------------------------------------------------
+    extern task predictor();
+    // -------------------------------------------------------------------------
+    //  task : comparator
+    // -------------------------------------------------------------------------
+    // Description:
+    //   Compare the expected packet sent from predictor with actual sent from
+    //   DUT. Reports error if they don't match.
+    //   huge packet.   
+    // Arguments: None
+    //
+    // -------------------------------------------------------------------------    
+    extern task comparator();
+
+    extern task pred_track_txen();
+    extern task pred_track_rd();
+    extern task pred_track_underrun();
+    extern task pred_read_mem();
+    extern function pred_construct_data_pkt();
+    extern function pred_construct_control_pkt();
+    extern function pred_add_pad();
+    extern function pred_insert_pream();
+    extern function pred_check_len_4();
+    extern function pred_check_huge();
+    extern function logic [31:0] pred_calc_crc32(byte data[], int len);
+    extern function void comp_check_crc(eth_tx_pending_record rec, int payload_len);
+endclass : eth_tx_scoreboard
+
+// =============================================================================
+//  IMPLEMENTATION
+// =============================================================================
+
+// function: new
+function eth_tx_scoreboard::new(string name, uvm_component parent);
+    super.new(name, parent);
+endfunction
+
+
+// function: build_phase
+function void eth_tx_scoreboard::build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    // Build fifos
+    wb_m_fifo     = new("wb_m_fifo",this);
+    mii_tx_fifo   = new("mii_tx_fifo",this);
+    // Build analysis exports
+    wb_m_a_export   = new("wb_m_export",this);
+    mii_tx_a_export = new("mii_tx_export",this);
+endfunction
+
+// function: connect_phase
+function void eth_tx_scoreboard::connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    // Connect each export wu=ith it's corrosponding fifo
+    wb_m_a_export.connect(wb_m_fifo.analysis_export);
+    mii_tx_a_export.connect(mii_tx_fifo.analysis_export);
+endfunction    
+
+// task: run_phase
+task eth_tx_scoreboard::run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    phase.raise_objection(this);
+    fork: fork_run_phase 
+        predictor();
+        comparartor();
+    join    
+    phase.drop_objection(this);
+endtask
+
+
+
+`endif // ETH_TX_SCOREBOARD_SV
