e4fb067359ba1f7a9e7680eefa1eb05a354af1a2
Mohammad Wael <102371006+mwael2002@users.noreply.github.com>
2026-07-01 16:36:14 +0300

    Add files via upload

--- tb/global/eth_glob_pkg.sv
+++ tb/global/eth_glob_pkg.sv
@@ -17,14 +17,23 @@ package eth_glob_pkg;
     // Enum represents write/read
     typedef enum logic { WB_READ = 1'b0, WB_WRITE = 1'b1 , UNKNOWN= 1'bx, HIGH_IMP= 1'bz} wb_dir_t;    
 
-    // Macros for width
-    parameter WB_DATA_WIDTH       = 32;
-    parameter WB_S_ADDR_WIDTH     = 10;
-    parameter WB_M_ADDR_WIDTH     = 32; 
-    parameter WB_SEL_WIDTH        = 4;
-    parameter PHY_NIBBLE_WIDTH    = 4;
+    // Queue for storing packet in bytes
+    typedef byte bytes_q[$];
 
-    parameter PHY_TX_CLK_FREQ_MHZ = 100;
+    // parameters
+    parameter WB_DATA_WIDTH          = 32;
+    parameter WB_S_ADDR_WIDTH        = 10;
+    parameter WB_M_ADDR_WIDTH        = 32; 
+    parameter WB_SEL_WIDTH           = 4;
+    parameter PHY_NIBBLE_WIDTH       = 4;
+    parameter ETH_PAUSE_FRAME_ADDR   = 48'h0180C2000001;                       // source address of PAUSE frame is hardcoded
+    parameter ETH_PAUSE_LEN_TYPE     = 16'h8808;
+    parameter ETH_PAUSE_OPCODE       = 16'h0001;    
+    parameter ETH_PAUSE_PAD          = 8'h00;
+    parameter ETH_PREAMBLE           = 8'h55;
+    parameter ETH_SFD                = 8'h5D;
+    
+    parameter PHY_TX_CLK_FREQ_MHZ    = 100;
 
     `include "dma_mem.sv"
 
