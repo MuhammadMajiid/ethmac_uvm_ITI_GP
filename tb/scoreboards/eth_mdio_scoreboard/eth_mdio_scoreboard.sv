fa475fdcbe5d0275c0b3e56f70644e7547a4e01d
Mohammad Wael <mwael5536@gmail.com>
2026-07-11 17:58:04 +0300

    updated mdio scoreboard

--- tb/scoreboards/eth_mdio_scoreboard/eth_mdio_scoreboard.sv
+++ tb/scoreboards/eth_mdio_scoreboard/eth_mdio_scoreboard.sv
@@ -14,25 +14,23 @@
 
 
 typedef struct {
-    mdio_seq_item_base::op_code_e op_code;
-    bit        mii_no_pre;
-    bit [7:0]  clk_div;
-    bit        w_ctrl_data;
-    bit        r_stat;
-    bit        scan_stat;
-    bit [4:0]  reg_addr;
-    bit [4:0]  phy_addr;
-    bit [15:0] wr_data;
-    bit [15:0] rd_data;
-    bit [1:0]  invalid;
-    bit        busy; 
-    bit        link_fail;
+    mdio_seq_item_base::op_code_e   op_code;
+    bit                             mii_no_pre;
+    bit [ETH_CTRL_CLK_DIV_LEN-1:0]  clk_div;
+    bit                             w_ctrl_data;
+    bit                             r_stat;
+    bit                             scan_stat;
+    bit [ETH_CTRL_ADDR_LEN-1:0]     reg_addr;
+    bit [ETH_CTRL_ADDR_LEN-1:0]     phy_addr;
+    bit [ETH_CTRL_ADDR_LEN-1:0]     wr_data;
+    bit [ETH_CTRL_DATA_LEN-1:0]     rd_data;
+    bit                             invalid;
+    bit                             busy; 
+    bit        l                    ink_fail;
 } mdio_cfg_reg_s;
 
 class eth_mdio_scoreboard extends uvm_scoreboard;
   `uvm_component_utils(eth_mdio_scoreboard)
-
-  parameter SEM_NO_KEYS = 3;
 
 
   // =========================================================================
@@ -56,10 +54,6 @@ class eth_mdio_scoreboard extends uvm_scoreboard;
   // =========================================================================
   eth_reg_block                                     m_regmodel;   
   // =========================================================================
-  // Semaphore for getting mii tx transaction from fifo
-  // =========================================================================
-  semaphore                                         m_sem; 
-  // =========================================================================
   // Event for ending packet comparison
   // =========================================================================
   event                                             m_ev_end_comp;  
@@ -82,9 +76,11 @@ class eth_mdio_scoreboard extends uvm_scoreboard;
   
   extern task get_seq_item();
   extern task read_cfg_regs();
+  extern task read_stat_regs();
   extern task pred_write(input mdio_seq_item_base item);
   extern task comp_write(input mdio_seq_item_base item);
-
+  extern task pred_read();
+  extern task comp_read();
 
 endclass
 
@@ -100,7 +96,6 @@ endfunction
 function void eth_mdio_scoreboard::build_phase(uvm_phase phase);
     super.build_phase(phase);
     a_fifo = new("a_fifo", this);
-    m_sem  =new(SEM_NO_KEYS);
     // get config object from database
     if (!uvm_config_db #(eth_mdio_scoreboard_config)::get(this, "", "config", m_config))
       `uvm_error(get_type_name(), "eth_mdio_scoreboard_config not found in config_db")
@@ -119,22 +114,49 @@ endfunction
 task run_phase(uvm_phase phase);
   
       phase.raise_objection(this);
-      fork
-      get_seq_item();
-      #0 comp_read();
-      #0 comp_clk_period();
-
+      fork: fork_mdio
       begin
         forever
-          begin
-            // Read cfgs
-            // Write data
-            if(m_cfg_reg_s.wr_data) 
-              wait(m_ev_end_comp.triggered);
-            pred_scan();  
+        begin
+            get_seq_item();
+            read_cfg_regs();
+            if(m_cfg_reg_s.w_ctrl_data) begin 
+                do begin
+                  #1;
+                  read_stat_regs();
+                end
+                while(m_cfg_reg_s.busy);
+                pred_write(m_mdio_seq_item);
+                comp_write(m_mdio_seq_item);
+            end  
+            else if(m_cfg_reg_s.scan_stat) begin 
+                do begin
+                  #1;
+                  read_stat_regs();
+                end
+                while(m_cfg_reg_s.invalid);
+                pred_read();
+                comp_read();
+            end  
+            else if(m_cfg_reg_s.r_stat) begin 
+                do begin
+                  #1;
+                  read_stat_regs();
+                end
+                while(m_cfg_reg_s.busy);
+                pred_read();
+                comp_read();
+                // comp linkfail
+              end  
 
-          end  
-        end
+            comp_clk_period();   
+            -> m_ev_end_comp;          
+        end  
+      end
+      begin
+        wait(m_ev_end_comp.triggered);
+        disable fork_mdio;
+      end
       join;
       
       phase.drop_objection(this);
@@ -143,16 +165,10 @@ task run_phase(uvm_phase phase);
 endtask
 
 task eth_mdio_scoreboard::get_seq_item();
-    forever begin
-        // Get all keys from semaphore
-        repeat(SEM_NO_KEYS)
-        m_sem.get(1);
         // Get transaction item from fifo
         a_fifo.get(m_mdio_seq_item);
         `uvm_info(get_type_name(),m_mdio_seq_item.convert2string(),UVM_HIGH)
-        // Put all Keys in semaphore
-        m_sem.put(SEM_NO_KEYS);
-    end
+
 endtask 
 
 task eth_mdio_scoreboard::read_cfg_regs();
@@ -175,15 +191,17 @@ task eth_mdio_scoreboard::read_cfg_regs();
   m_regmodel.MIITX_DATA.mirror(status, UVM_CHECK, UVM_BACKDOOR);
   m_cfg_reg_s.wr_data = m_regmodel.MIITX_DATA.CTRLDATA.get_mirrored_value();
 
-  m_regmodel.MIIRX_DATA.mirror(status, UVM_CHECK, UVM_BACKDOOR);
+endtask
+
+task read_stat_regs();
+   m_regmodel.MIIRX_DATA.mirror(status, UVM_CHECK, UVM_BACKDOOR);
   m_cfg_reg_s.rd_data = m_regmodel.MIIRX_DATA.PRSD.get_mirrored_value();
 
   m_regmodel.MIISTATUS.mirror(status, UVM_CHECK, UVM_BACKDOOR);
   m_cfg_reg_s.invalid   = m_regmodel.MIISTATUS.NVALID.get_mirrored_value();
   m_cfg_reg_s.busy      = m_regmodel.MIISTATUS.BUSY.get_mirrored_value();
   m_cfg_reg_s.link_fail = m_regmodel.MIISTATUS.LINKFAIL.get_mirrored_value();
-
-endtask
+endtask  
 
 task eth_mdio_scoreboard::pred_write();
   // Check if no preamble is disabled then push preamble
@@ -270,11 +288,7 @@ task eth_mdio_scoreboard::comp_write(input mdio_seq_item_base item);
   int idx =0;
   bit err =0;
   bit temp[$];
-  forever begin
-    if(!m_cfg_reg_s.w_ctrl_data) begin
-      #1;
-      continue;
-    end  
+ 
     // Compare preamble
     if(!m_cfg_reg_s.mii_no_pre) begin
       temp=m_exp_wr_pkt[idx:ETH_CTRL_PREAMBLE_LEN-1];
@@ -345,11 +359,7 @@ task eth_mdio_scoreboard::comp_write(input mdio_seq_item_base item);
         err =1;
     end
     if(!err)
-        `uvm_info(get_type_name(),"Management Packet comparison PASSED",UVM_LOW)
-
-    -> m_ev_end_comp;
-      #1ns;
-    end
+        `uvm_info(get_type_name(),"Management Write Packet comparison PASSED",UVM_LOW)
 
 endtask
 
@@ -363,7 +373,7 @@ task comp_read();
         #1;
         continue;
       end  
-      m_sem.get(1);
+
       // Compare preamble
       if(!m_cfg_reg_s.mii_no_pre) begin
         temp=m_exp_rd_pkt[idx:ETH_CTRL_PREAMBLE_LEN-1];
@@ -436,24 +446,20 @@ task comp_read();
           err =1;
       end
       if(!err)
-          `uvm_info(get_type_name(),"Management Packet comparison PASSED",UVM_LOW)
+          `uvm_info(get_type_name(),"Management Read Packet comparison PASSED",UVM_LOW)
       
-      m_sem.put(1); 
-      -> m_ev_end_comp;
-      #1ns;
     end
 endtask
 
 task comp_clk_period();
     real exp_period_ns;
-    forever begin
+
       // check if the division value is 0
       if(m_cfg_reg_s.clk_div==0) begin
           `uvm_error(get_type_name(),"Clock frequency divider = 0",UVM_LOW)
-          #1;
           return;
       end  
-      m_sem.get(1);
+
       // Calculate expected period , check if it's even or odd as the calculation differs
       exp_period_ns=(m_cfg_reg_s.clk_div%2==0)?(WB_CLK_PERIOD_NS*m_cfg_reg_s.clk_div):(WB_CLK_PERIOD_NS*(m_cfg_reg_s.clk_div-1)):
       if(exp_period_ns!=m_mdio_seq_item.clk_period_ns) 
@@ -462,10 +468,7 @@ task comp_clk_period();
           $sformatf("clock period mismatch, Divisor value = %0d, clk periods in ns:\n Wishbone = %0f \n Expexcted Management = %0f \n Actual Management = %0f",
           m_cfg_reg_s.clk_div,WB_CLK_PERIOD_NS,exp_period_ns,m_mdio_seq_item.clk_period_ns))
       end
-      
-      m_sem.put(1); 
-      #1ns;
-    end
+
 endtask
 
 `endif // ETH_MDIO_SCOREBOARD_SV
