e1f73abb22618fbe252219a87b7b0ded90d11471
Mohammad Wael <mwael5536@gmail.com>
2026-08-22 19:12:34 +0300

    updated README

--- results/tx/coverage/eth_test_miim_bitcounter_abort_sweep_html_func/files/tests.js
+++
@@ -1,2 +0,0 @@
-var g_data = {"0":"eth_test_miim_bitcounter_abort_sweep_func_cov"};
-processTestsData(g_data);
--- results/tx/coverage/eth_test_miim_clear_cmd_while_busy_html_func/files/tests.js
+++
@@ -1,2 +0,0 @@
-var g_data = {"0":"eth_test_miim_clear_cmd_while_busy_func_cov"};
-processTestsData(g_data);
